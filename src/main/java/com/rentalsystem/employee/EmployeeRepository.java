2d62d90a1028370921f18882934f513c83d86a0e
Prashan Nethsara Ranasinghe <erisgod50@gmail.com>
2026-05-17 20:41:39 +0530

    Update EmployeeRepository.java

--- src/main/java/com/rentalsystem/employee/EmployeeRepository.java
+++ src/main/java/com/rentalsystem/employee/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.rentalsystem.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+  List<Employee> findByNameContainingIgnoreCase(String name);
 }
